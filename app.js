1016531c7c5ece5efd006976bb61c78205a9ba22
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-03-11 15:52:04 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express=require('express');
+const router=express.Router();
 const app=express();
-app.set('port',process.env.PORT || 7700);
+const data=[];
+app.set('port',process.env.PORT);
+console.log(`portname is ${process.env.PORT}`);
+console.log(`Hostname is ${process.env.HOST}`);
+data.push(process.env.PORT);
+data.push(process.env.HOST);
 app.get('/',function(req,res){
-  res.send(`Welcome!!!!`);
+    res.send(`${data}   Hello World`);
 });
 const server=app.listen(app.get('port'),function(){
   console.log(`Express running in port ${server.address().port}`);
+ 
 });
