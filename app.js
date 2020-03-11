1c089e31ded8fc37b477537dd645fb613760c5df
pachputeshrutika21 <52059643+pachputeshrutika21@users.noreply.github.com>
2020-03-11 11:55:52 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
+const express=require('express');
+const app=express();
 app.set('port',process.env.PORT || 7700);
-app.get('/', function (req, res) {
-  res.send('Welcome!!!!');
+app.get('/',function(req,res){
+  res.send(`Welcome!!!!`);
 });
-const server=app.listen(app.get('port'), function () {
-  console.log(`Example app listening on port ${server.address().port}`);
+const server=app.listen(app.get('port'),function(){
+  console.log(`Express running in port ${server.address().port}`);
 });
